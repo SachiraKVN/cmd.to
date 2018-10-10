0b0b866c7aa4d61958be3db6458f9053858c22d1
Nuwan <nuwanchamp02@gmail.com>
2018-10-11 00:29:17 +0530

    Desktop icon creation on install

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 
-!#/usr/bin/bash
+#!/usr/bin/bash
 
 # Create a folder in bin directory
+current_directory=$(pwd);
 
-sudo cp -vf ./cmd /usr/bin
+chmod -R 777 "$current_directory/"
+replacement="$HOME"
+sed -i "s:\~:${replacement}:g" "$current_directory/cmd.desktop" &2 > /dev/null
+cp  "$current_directory/cmd.desktop" $HOME/Desktop
+sed 
+echo 'coping ...'
+cp -a  $current_directory $HOME/.cmd.to
+
+# Add executeable to bin directory
+ sudo cp -f ./cmd /usr/bin
+ 
+ exit
+
+# Get current working directory
+# echo "$(dirname "$(pwd)/$?")"
+
+
